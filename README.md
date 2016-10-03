f7f089f64bb1d7d223cdb857fb8973d83a0958cb
Code Ass <aycabta@gmail.com>
2016-10-04 02:06:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+YouTube seeker
+==============
+
+YouTube seeker with benry.
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/YouTube_seeker)
+
+## Usage
+
+In YouTube video, you can use some key commands below:
+
+<dl>
+<dt><code>u</code></dt><dd>Seeks to seconds ago (defualt 5 seconds)</dd>
+<dt><code>o</code></dt><dd>Seeks to seconds later (default 5 seconds)</dd>
+<dt><code>[</code></dt><dd>Increments seek duration for <code>u</code> and <code>o</code></dd>
+<dt><code>]</code></dt><dd>Decrements seek duration for <code>u</code> and <code>o</code></dd>
+<dt><code>s</code></dt><dd>Saves present time for seeking with <code>r</code></dd>
+<dt><code>r</code></dt><dd>Seeks to the time what is saved by <code>s</code></dd>
+</dl>
---
+++ youtube-seeker.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        YouTube seeker
+// @namespace  http://aycabta.github.io/
+// @version    0.1.0
+// @description  benry
+// @include    /^https:\/\/www\.youtube\.com\/watch\?v=/
+// @copyright  2016+, Code Ass
+// ==/UserScript==
+
+(function() {
+    var player = document.getElementById("movie_player");
+    var savedTime = null;
+    var seekSeconds = 5;
+    document.addEventListener("keydown", function(e) {
+        var to = null;
+        switch (e.keyCode) {
+            case 219: // [
+                seekSeconds--;
+                console.log("seek with " + seekSeconds + " seconds");
+                break;
+            case 221: // ]
+                seekSeconds++;
+                console.log("seek with " + seekSeconds + " seconds");
+                break;
+            case 85: // u
+                to = player.getCurrentTime() - seekSeconds;
+                break;
+            case 79: // o
+                to = player.getCurrentTime() + seekSeconds;
+                break;
+            case 83: // s
+                savedTime = player.getCurrentTime();
+                break;
+            case 82: // r
+                if (savedTime !== null) {
+                    to = savedTime;
+                }
+                break;
+        }
+        if (to !== null) {
+            if (to < 0) {
+                to = 0;
+            } else if (to > player.getDuration()) {
+                to = player.getDuration();
+            }
+            player.seekTo(to, true);
+        }
+    });
+})();
