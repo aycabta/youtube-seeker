980c66a6010ca551795f1524f6fbe2d43366c78a
aycabta <aycabta@gmail.com>
2018-03-04 00:20:26 +0900

    Fix for latest Greasemonkey and YouTube

--- youtube-seeker.user.js
+++ youtube-seeker.user.js
@@ -6,75 +6,89 @@
 // @description  benry
 // @include    /^https:\/\/www\.youtube\.com\/watch\?.*v=.*$/
 // @copyright  2016+, Code Ass
+// @grant       GM_setValue
 // ==/UserScript==
 
 (function() {
-    var player = document.getElementById("movie_player");
-    var savedTime = null;
-    var seekSeconds = 5;
-    var isShownControls = true;
-    var leftControls = document.getElementsByClassName('ytp-left-controls')[0];
-    var secondsInfo = document.createElement('div');
-    secondsInfo.style.color = '#BBB';
-    secondsInfo.classList.add('ytp-time-display');
-    secondsInfo.classList.add('notranslate');
-    var secondsText = document.createTextNode('seek ' + seekSeconds.toString(10))
-    secondsInfo.appendChild(secondsText);
-    leftControls.appendChild(secondsInfo);
-    document.addEventListener("keydown", function(e) {
-        var to = null;
-        switch (e.keyCode) {
-            case 219: // [
-                seekSeconds--;
-                console.log("seek with " + seekSeconds + " seconds");
-                secondsText.textContent = 'seek ' + seekSeconds.toString(10);
-                break;
-            case 221: // ]
-                seekSeconds++;
-                console.log("seek with " + seekSeconds + " seconds");
-                secondsText.textContent = 'seek ' + seekSeconds.toString(10);
-                break;
-            case 85: // u
-                to = player.getCurrentTime() - seekSeconds;
-                break;
-            case 79: // o
-                to = player.getCurrentTime() + seekSeconds;
-                break;
-            case 83: // s
-                savedTime = player.getCurrentTime();
-                break;
-            case 82: // r
-                if (savedTime !== null) {
-                    to = savedTime;
-                }
-                break;
-            case 72: // h
-                if (isShownControls) {
-                    player.hideControls();
-                    isShownControls = false;
-                } else {
-                    player.showControls();
-                    isShownControls = true;
-                }
-                break;
-            case 73: // i ...secret feature
-                switch (player.getPlayerState()) {
-                    case 1: // playing
-                        player.pauseVideo();
-                        break;
-                    default:
-                        player.playVideo();
-                        break;
-                }
-                break;
-        }
-        if (to !== null) {
-            if (to < 0) {
-                to = 0;
-            } else if (to > player.getDuration()) {
-                to = player.getDuration();
+    function initialize() {
+        var savedTime = null;
+        var seekSeconds = 5;
+        var isShownControls = true;
+        var leftControls = document.getElementsByClassName('ytp-left-controls')[0];
+        var secondsInfo = document.createElement('div');
+        secondsInfo.style.color = '#BBB';
+        secondsInfo.classList.add('ytp-time-display');
+        secondsInfo.classList.add('notranslate');
+        var secondsText = document.createTextNode('seek ' + seekSeconds.toString(10))
+        secondsInfo.appendChild(secondsText);
+        leftControls.appendChild(secondsInfo);
+        document.addEventListener("keydown", function(e) {
+            var to = null;
+            var player = document.getElementById("movie_player");
+            switch (e.keyCode) {
+                case 219: // [
+                    seekSeconds--;
+                    console.log("seek with " + seekSeconds + " seconds");
+                    secondsText.textContent = 'seek ' + seekSeconds.toString(10);
+                    break;
+                case 221: // ]
+                    seekSeconds++;
+                    console.log("seek with " + seekSeconds + " seconds");
+                    secondsText.textContent = 'seek ' + seekSeconds.toString(10);
+                    break;
+                case 85: // u
+                    to = player.getCurrentTime() - seekSeconds;
+                    break;
+                case 79: // o
+                    to = player.getCurrentTime() + seekSeconds;
+                    break;
+                case 83: // s
+                    savedTime = player.getCurrentTime();
+                    break;
+                case 82: // r
+                    if (savedTime !== null) {
+                        to = savedTime;
+                    }
+                    break;
+                case 72: // h
+                    if (isShownControls) {
+                        player.hideControls();
+                        isShownControls = false;
+                    } else {
+                        player.showControls();
+                        isShownControls = true;
+                    }
+                    break;
+                case 73: // i ...secret feature
+                    switch (player.getPlayerState()) {
+                        case 1: // playing
+                            player.pauseVideo();
+                            break;
+                        default:
+                            player.playVideo();
+                            break;
+                    }
+                    break;
             }
-            player.seekTo(to, true);
-        }
-    });
+            if (to !== null) {
+                if (to < 0) {
+                    to = 0;
+                } else if (to > player.getDuration()) {
+                    to = player.getDuration();
+                }
+                console.log(to);
+                console.log(player);
+                player.seekTo(to, true);
+            }
+        });
+    };
+
+    if (window.top === window.self) {
+        var script = document.createElement ('script');
+        script.textContent = '(' + initialize.toString() + ')()';
+        var head = document.getElementsByTagName ('head')[0];
+        setTimeout(function() {
+            head.appendChild(script);
+        }, 200);
+    }
 })();
